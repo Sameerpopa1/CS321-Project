7a8cd1e74105c53ca6492ee795cf577734dee931
Sameerpopa1 <sameerpopal36@gmail.com>
2026-04-30 01:51:09 -0400

    Handles Firestore CRUD for per-user favorites

--- FavoritesRepository.js
+++ FavoritesRepository.js
@@ -2,6 +2,7 @@
 // Single responsibility: persist and retrieve user favorites.
 // Stored as a subcollection: users/{userId}/favorites
 
+
 import { db } from "./firebase-config.js";
 import {
   collection, doc, getDocs, getDoc, setDoc, deleteDoc
